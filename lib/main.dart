514c7a1c83d46e91ae07473039e9704a9fa8af10
Akshansh Bhanjana <akshansh2000@gmail.com>
2019-08-02 11:44:26 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(
+      MaterialApp(
+        home: ConvokeApp(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+
+class ConvokeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
